77a4b4bd64e7bbf9d5bedeb1717ebc251ff90402
pszklarska <paulina.szklarska@droidsonroids.pl>
2018-02-04 09:47:47 +0100

    Adding progress indicator if list is not loaded yet and error if list has errors.

--- app/lib/ui/main.dart
+++ app/lib/ui/main.dart
@@ -28,10 +28,31 @@ class AppScreen extends StatelessWidget {
           title: new Text(Strings.APP_NAME),
         ),
         body: new FutureBuilder(
-            future: restManager.loadRepositories(), builder: _buildList));
+            future: restManager.loadRepositories(),
+            builder: _buildWidgetFromRepoListState));
   }
 
-  Widget _buildList(BuildContext context, AsyncSnapshot<List<Repo>> snapshot) {
+  Widget _buildWidgetFromRepoListState(
+      BuildContext context, AsyncSnapshot<List<Repo>> snapshot) {
+    switch (snapshot.connectionState) {
+      case ConnectionState.none:
+      case ConnectionState.waiting:
+        return _buildProgress();
+      default:
+        if (snapshot.hasError) {
+          return _buildError();
+        } else {
+          return _buildRepoList(context, snapshot);
+        }
+    }
+  }
+
+  Center _buildProgress() => new Center(child: new CircularProgressIndicator());
+
+  Center _buildError() => new Center(child: new Icon(Icons.error));
+
+  Widget _buildRepoList(
+      BuildContext context, AsyncSnapshot<List<Repo>> snapshot) {
     List<Repo> repoList = snapshot.data;
     return new ListView.builder(
         itemBuilder: (context, index) =>
